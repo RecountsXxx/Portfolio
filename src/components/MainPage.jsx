c330f675523672da4c2814771085c46980b235e9
Bogdan <Bogdan@DESKTOP-PGENPK7>
2023-07-15 00:55:53 +0300

    added responsive design(release)

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -23,9 +23,9 @@ class MainPage extends Component{
         return(
           <div className="container-fluid">
               <div id="about" className="about-us d-flex justify-content-center mt-5">
-                  <div className="ms-5 mt-5">
-                      <h1 className="text-white flow-around">Bogdan Kapriyan Junior Front-end developer</h1>
-                      <h4 className="text-white-50 ms-5 flow-around">Hello! My name is Bogdan Kapryan, and I am a junior frontend developer. I have knowledge of frameworks such as React, Angular, and Vue. Although I am a junior developer, I am familiar with these frameworks and can use them to build scalable and interactive user interfaces.
+                  <div className="about-div">
+                      <h1 className="text-opacity-75 text-white flow-around">Bogdan Kapriyan <span className="text-opacity-75 text-white ">(Junior Front-end developer)</span></h1>
+                      <h4 className="text-white-50 flow-around">Hello! My name is Bogdan Kapryan, and I am a junior frontend developer. I have knowledge of frameworks such as React, Angular, and Vue. Although I am a junior developer, I am familiar with these frameworks and can use them to build scalable and interactive user interfaces.
 
                           I also have experience with CSS frameworks like Bootstrap. I can create stylish and responsive web pages, applying modern design techniques and responsiveness.
 
@@ -33,14 +33,14 @@ class MainPage extends Component{
 
                           Although I am a junior developer, I am constantly working on improving my skills and staying updated with the latest trends in frontend development. I am eager to take on new challenges and participate in exciting projects where I can apply my knowledge and creativity.</h4>
                   </div>
-                  <img src={square} alt="..."/>
+                  <img className="decoration-img" src={square} alt="..."/>
               </div>
 
               <div className="projects mt-5">
                   <h1 className="text-white flow-around">Projects</h1>
                   <div id="projects"  className="projects-grid">
                           <div className="d-flex justify-content-center">
-                              <img src="https://img.icons8.com/cute-clipart/512/chat.png" alt="..."/>
+                              <img src="https://img.icons8.com/color/480/chat--v1.png" alt="..."/>
                               <h3 className="mt-2">Chater(С#)</h3>
                               <h5 className="text-secondary">Chater is an advanced messaging application that allows users to chat, call other friends and share files. Our application offers a convenient and secure way to communicate, a choice of functions and customization for specific users. The application is written in, c# server is tcp client, audio calls are c# is udp client, c# main application is tcp client.</h5>
                               <a href="https://github.com/RecountsXxx/chater">Look it up</a>
@@ -115,7 +115,7 @@ class MainPage extends Component{
                       </div>
                   </div>
                   <div className="mt-5">
-                      <h2 className="text-white">Additional technologies and skills</h2>
+                      <h2 className="text-white addition-text">Additional technologies and skills</h2>
                       <ul className="ms-5 mt-2 ul-list text-white">
                           <li className="list-item">Git</li>
                           <li className="list-item">Quick learning</li>
